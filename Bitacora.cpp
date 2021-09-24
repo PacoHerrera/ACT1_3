9bc3d6c1c080532562a6188e35a3c2b6e2b11c25
Francisco_Herrera <pacoherrera218@gmail.com>
2021-09-23 20:02:45 -0500

    update

---
+++ Bitacora.cpp
@@ -0,0 +1,15 @@
+#include "Bitacora.hpp"
+
+
+ostream & operator<<(ostream & os, const Bitacora & bitacora)
+{
+    os << bitacora.date << " - ";
+    os << bitacora.time << " - ";
+    os << bitacora.IP_O << " - ";
+    os << bitacora.OP << " - ";
+    os << bitacora.ON << " - ";
+    os << bitacora.DP << " - ";
+    os << bitacora.IP_D << " - ";
+
+    return os;
+}
