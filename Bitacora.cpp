0a893775eecbd1c35343120eab814721f0e2c56a
Francisco_Herrera <pacoherrera218@gmail.com>
2021-09-23 19:42:07 -0500

    update

--- Bitacora.cpp
+++ Bitacora.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 class Bitacora{
     private:
+        int date;
         int time;
+        int IP_O;
+        int PO;
+        string ON;
+        int IP_D;
+        int DP;
+        string DN;
 };
