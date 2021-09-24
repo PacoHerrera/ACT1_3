a0a26c391d72fbb2c119bbf3140df5ca506bf484
Francisco_Herrera <pacoherrera218@gmail.com>
2021-09-23 21:36:35 -0500

    update

--- Registro.cpp
+++ Registro.cpp
@@ -1,2 +1,10 @@
 #include "Registro.hpp"
 
+string Registro::Date(){return date;}
+string Registro::Time(){return time;}
+string Registro::originIp(){return IP_O;}
+string Registro::originPort(){return OP;}
+string Registro::originName(){return ON;}
+string Registro::destinationPort(){return DP;}
+string Registro::destinationIp(){return IP_D;}
+string Registro::destinationName(){return DP;}
