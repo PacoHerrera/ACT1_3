625693cb32dc68eaa392778791c1fd78fbfdf578
Francisco_Herrera <pacoherrera218@gmail.com>
2021-09-23 20:40:53 -0500

    update

--- Registro.cpp
+++ Registro.cpp
@@ -1,15 +1,2 @@
 #include "Registro.hpp"
 
-
-ostream & operator<<(ostream & os, const Registro & registro)
-{
-    os << registro.date << " - ";
-    os << registro.time << " - ";
-    os << registro.IP_O << " - ";
-    os << registro.OP << " - ";
-    os << registro.ON << " - ";
-    os << registro.DP << " - ";
-    os << registro.IP_D << " - ";
-
-    return os;
-}
